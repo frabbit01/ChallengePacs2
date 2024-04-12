e249458d353626b6fc9bf742a40f2e80341da988
frabbit01 <francesca.visalli@mail.polimi.it>
2024-04-12 14:56:41 +0000

    Update

--- Matrix.hpp
+++ Matrix.hpp
@@ -9,17 +9,18 @@ namespace algebra{
 
     template<typename T,typename StorageOrder> class Matrix{
         public:
-            void compress();
-            void uncompress();
+            void compress();//Bisogna generalizzare per il caso in cui lo StorageOrder non sia per CSR
+            void uncompress(); //Implementazione da finire!!!!
             bool is_compressed() const{return compressed;}
             //call operator:Missing
             //constructor
             Matrix()=default; //default constructor
+            //Bisogna generalizzare per il caso in cui lo StorageOrder non sia per CSR!!
             Matrix(std::size_t _n_rows,std::size_t _n_cols):  //constructor that takes size of the matrix as input
-            n_rows(_n_rows){
+            n_rows(_n_rows),n_cols(_n_cols),compressed(false){
                 T default_t;
                 for(std::size_t i=0;i<n_rows;++i){
-                    for(std::size_t j=0;j<_n_cols;++j){
+                    for(std::size_t j=0;j<n_cols;++j){
                         std::array<std::size_t,2> key={i,j};
                         COOmap[key]=default_t;
                     }
@@ -28,7 +29,8 @@ namespace algebra{
             //resize
         private:
             //CSR format
-            unsigned n_rows; //Initialized in the constructor
+            unsigned n_rows; //Initialized in the constructor (non default)
+            unsigned n_cols; //Initialized in the constructor (non default)
             unsigned n_nnz; //I want to update this every time I use the call operator
             std::vector<std::size_t> inner_indices; //I initialize this in the uncompress method; starting index for the element of each row
             std::vector<std::size_t> outer_indices; //I initialize this in the uncompress method; corresponding column idxs
--- Matrix_impl.hpp
+++ Matrix_impl.hpp
@@ -3,18 +3,22 @@
 
 //implemenation of Matrix class methods 
 #include "Matrix.hpp"
+#include<utility>
 
 namespace algebra{
     template<typename T,typename StorageOrder>
     void
     Matrix<T,StorageOrder>::compress(){
+        if(compressed) //If the matrix is already compressed I exit the function
+            return;
         inner_indices=std::vector<std::size_t>(n_rows+1);
         outer_indices=std::vector<std::size_t>(n_nnz);
         values=std::vector<T>(n_nnz);
         std::size_t count=0,i=0,row=0;
+        bool start_row=false;//flag to check when the first non null element of a row is
         for(auto iter=COOmap.cbegin();iter!=COOmap.cend();++iter){
             if(iter->first[0]!=row) //I reset the flag when I go to the next row in the matrix
-                bool start_row=false; //flag to check when the first non null element of a row is
+                start_row=false; 
             if(iter->second!=0){
                 values[i]=iter->second;//store nnz value
                 outer_indices=iter->first[1];//store column index
@@ -27,6 +31,19 @@ namespace algebra{
             }
         }
         COOmap.clear(); //I clear the map for the COOmap format
+        compressed=true;
+    }
+
+    template<typename T,typename StorageOrder>
+    void
+    Matrix<T,StorageOrder>::uncompress(){
+        if(!compressed)
+            return;
+        for(std::size_t i=0;i<n_rows;++i){
+            for(size_t j=0;j<n_cols;++j){ //I know n_cols will have to have been initialized either in the call operator (non const) or in the default constructor, since I cannot add elements in the compressed state
+                //finire dopo aver definito il call operator
+            }
+        }
     }
 };
 
