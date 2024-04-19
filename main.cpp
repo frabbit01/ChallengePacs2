a07c0700251bdd54d90959027cdf51f2cc46a7c8
frabbit01 <francesca.visalli@mail.polimi.it>
2024-04-19 09:04:09 +0000

    update: matrix reader seems to work correctly, and so do the call operators

--- main.cpp
+++ main.cpp
@@ -5,17 +5,11 @@
 #include<string>
 using namespace algebra;
 int main(){
-    //std::ifstream("/mnt/c/Users/hp/Desktop/uni/PACS/ChallengePacs2/lnsp_131.mtx.gz"); //this path will need to be changed depending on the location of the file
-    //I need to change the matrix reader with fstream functions
-    //std::string filename="lnsp_131.mtx";
-    const char * filename= "lnsp_131.mtx";
-    /*std::ifstream in(filename);
-    if(!in)
-        std::cerr<<"file cannot be opened"<<std::endl;*/
+    const char * filename= "lnsp_131.mtx"; //I used this object instead of a file stream since the library I used was originally written for C, so in order to avoid conflicts I used fopen and fclose
     Matrix<double,StorageOrder::Rows> matrix;
     matrix=matrix.read_market_matrix(filename);
     std::cout<<"yay"<<std::endl;
     std::cout<<matrix.nnz()<<std::endl;
-    std::cout<<matrix(1,1)<<std::endl;
+    std::cout<<matrix(43,34)<<std::endl;
     return 0;
 }
