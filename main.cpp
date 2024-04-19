062ab0b8217755bd6bcbd6bd0837740830c9df6a
frabbit01 <francesca.visalli@mail.polimi.it>
2024-04-19 10:38:58 +0000

    errors are to be fixed in the main

--- main.cpp
+++ main.cpp
@@ -7,9 +7,14 @@ using namespace algebra;
 int main(){
     const char * filename= "lnsp_131.mtx"; //I used this object instead of a file stream since the library I used was originally written for C, so in order to avoid conflicts I used fopen and fclose
     Matrix<double,StorageOrder::Rows> matrix;
-    matrix=matrix.read_market_matrix(filename);
-    std::cout<<"yay"<<std::endl;
-    std::cout<<matrix.nnz()<<std::endl;
-    std::cout<<matrix(43,34)<<std::endl;
+    matrix=matrix.read_market_matrix(filename); //here I read the matrix
+    std::cout<<matrix(43,34)<<std::endl; //here I check that the values and dimensions are correct
+    std::cout<<"rows: "<<matrix.rows()<<"\n columns: "<<matrix.columns()<<"\nnon-zero elems: "<<matrix.nnz()<<std::endl;
+
+    //here I test the same for a column order matrix
+    Matrix<double,StorageOrder::Columns> matrix_columns;
+    matrix_columns=matrix_columns.read_market_matrix(filename);
+    std::cout<<matrix(43,34)<<std::endl; //here I check that the values and dimensions are correct
+    std::cout<<"rows: "<<matrix.rows()<<"\n columns: "<<matrix.columns()<<"\nnon-zero elems: "<<matrix.nnz()<<std::endl;
     return 0;
 }
