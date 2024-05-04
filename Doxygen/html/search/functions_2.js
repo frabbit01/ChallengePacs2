27632c602b3ea14e8017cfa09a1e648c2fbfcb9b
frabbit01 <francesca.visalli@mail.polimi.it>
2024-05-04 08:59:32 +0000

    Provvisory Doxyfile

---
+++ Doxygen/html/search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['is_5fcompressed_37',['is_compressed',['../classalgebra_1_1Matrix.html#a8889930e0702930ccec966238c5960b6',1,'algebra::Matrix::is_compressed()'],['../classalgebra_1_1Matrix_3_01T_00_01StorageOrder_1_1Columns_01_4.html#a7780872f95b764aa1456dfc805423de2',1,'algebra::Matrix&lt; T, StorageOrder::Columns &gt;::is_compressed()']]]
+];
---
+++ Doxygen/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['challengepacs2_51',['ChallengePacs2',['../md_README.html',1,'']]]
+];
